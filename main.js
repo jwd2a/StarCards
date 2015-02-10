e0fa5a6030429b9cdb95f17d7d17cee5f3f82407
jwd2a <jdavis1002@gmail.com>
2015-02-10 16:04:02 -0500

    Shuffling cards and adding to the DOM

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-(function(){
+(function() {
 
   //Set up some configuration variables
+
+  var numMatches = 2;
 
   //TODO: make number of cards/pages recieved dynamic
 
@@ -8,24 +10,50 @@
 
   /* We need to do multiple API calls in a loop to get multiple pages. We'll calculate that based on the number of items we need against the number of items returned. */
 
-  var people = [];
   var count = 0;
+  var people = [];
 
-  for(i=0;i<2;i++){
+  for (i = 1; i < 3; i++) {
     var req = new XMLHttpRequest();
-    req.onload = function(){
+    req.onload = function() {
       people = people.concat(JSON.parse(this.responseText).results);
       count++;
-      if(count == 2){
-        buildDeck();
+      if (count == 2) {
+        buildDeck(people);
       }
     }
-    req.open("GET", "http://swapi.co/api/people/");
+    req.open("GET", "http://swapi.co/api/people/?page="+i);
     req.send();
   }
 
-  function buildDeck(){
-    console.log(people);
+  function buildDeck(people) {
+    var fullDeck = [];
+    for (i = 0; i < 15; i++){
+      for (x = 0; x < numMatches; x++){
+        console.log("Pushing "+people[i].name+" to the set");
+        fullDeck.push(people[i]);
+      }
+    }
+
+   buildCards(window.knuthShuffle(fullDeck)); 
+
+  }
+
+  function buildCards(shuffledArray) {
+    var cardCounter = 0;
+    var rowCounter = 0;
+    shuffledArray.forEach(function(person) {
+      if (cardCounter == 0) {
+        $("#cards").append("<div class='row' id='row-" + rowCounter + "'>");
+      }
+      $("#row-" + rowCounter).append("<div class='col-md-2 col-lg-2 card' data-itemId='" + person.name + "'>Back of card</div>");
+      cardCounter++;
+      if (cardCounter == 6) {
+        $("#row-" + rowCounter).append("</div>");
+        cardCounter = 0;
+        rowCounter++;
+      }
+    });
   }
 
 })();
